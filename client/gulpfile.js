d8208e06f2598f22364b385fcc42832fb108010b
eng-mostafa-sayed <ms519197@gmail.com>
2023-01-05 21:53:30 +0200

    gulp watching

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,3 +5,13 @@ const sass = gulpSass(require("sass"));
 gulp.task("sass", async () => {
   gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
 });
+
+//watch => it keep watching for any changes made
+gulp.task("watch", async function () {
+  await gulp.watch("src/components/**/*.scss", async function () {
+    gulp
+      .src("src/components/**/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("src/css"));
+  });
+});
