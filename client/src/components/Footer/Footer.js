b87c8fb3888d36289e9275d93108c862d2b1e7ed
eng-mostafa-sayed <ms519197@gmail.com>
2023-01-05 22:38:00 +0200

    add data json words.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../../css/Footer/Footer.css";
+import { words } from "../../words";
 const Footer = () => {
-  return <footer>footer</footer>;
+  return <footer>{words.footerTitle}</footer>;
 };
 
 export default Footer;
