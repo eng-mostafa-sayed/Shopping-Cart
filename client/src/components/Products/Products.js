53a98b71e2effde92e71847ffef85d76e46f24e9
eng-mostafa-sayed <ms519197@gmail.com>
2023-01-13 23:48:01 +0200

    animation

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "../../css/Products/Products.css";
 import ProductModal from "../productModal/productModal";
+import Bounce from "react-reveal/Bounce";
+
 function Products(props) {
   const [product, setProduct] = useState("");
 
@@ -13,21 +15,25 @@ function Products(props) {
   };
 
   return (
-    <div className="product-wrapper">
-      {props.products.map((product) => (
-        <div className="product-item" key={product.id}>
-          <a href="#" onClick={() => openModal(product)}>
-            <img src={product.imageUrl} alt={product.title}></img>
-          </a>
-          <div className="product-desc">
-            <p> {product.title}</p>
-            <span>{product.price}$</span>
+    <Bounce left cascade>
+      <div className="product-wrapper">
+        {props.products.map((product) => (
+          <div className="product-item" key={product.id}>
+            <a href="#" onClick={() => openModal(product)}>
+              <img src={product.imageUrl} alt={product.title}></img>
+            </a>
+            <div className="product-desc">
+              <p> {product.title}</p>
+              <span>{product.price}$</span>
+            </div>
+            <button onClick={() => props.addToCart(product)}>
+              Add to Cart
+            </button>
           </div>
-          <button onClick={() => props.addToCart(product)}>Add to Cart</button>
-        </div>
-      ))}
-      <ProductModal product={product} closeModal={closeModal} />
-    </div>
+        ))}
+        <ProductModal product={product} closeModal={closeModal} />
+      </div>
+    </Bounce>
   );
 }
 
