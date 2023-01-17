f13161b2ca316a12d376ae36fa055bc149c155dc
eng-mostafa-sayed <ms519197@gmail.com>
2023-01-17 15:06:02 +0200

    .

--- client/src/components/CheckoutForm/Checkout.jsx
+++ client/src/components/CheckoutForm/Checkout.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
-import "../../css/CheckoutForm/Checkout.css"
-import Input from '../Input/Input'
-import Zoom from 'react-reveal/Zoom'
+import React from "react";
+import "../../css/CheckoutForm/Checkout.css";
+import Input from "../Input/Input";
+import Zoom from "react-reveal/Zoom";
 function Checkout(props) {
-    return (
-        <>
-            {props.showForm &&  <div className="checkout-form">
-                <span className="close-icon" onClick={() => props.setShowForm(false)}> &times; </span> 
-                <Zoom left>
-                    <form onSubmit={props.submitOrder}>
-                        <Input
-                            label="Name"
-                            type="text"
-                            name="name"
-                            onChange={props.handleChange}
-                        />
-                        <Input
-                            label="Email"
-                            type="email"
-                            onChange={props.handleChange}
-                            name="email"
-                        />
-                                            
-                        <div>
-                            <button type="submit">  Checkout </button>
-                        </div>
-                    </form>
-                </Zoom>
-            </div>}
-        </>
-    )
+  return (
+    <>
+      {props.showForm && (
+        <div className="checkout-form">
+          <span className="close-icon" onClick={() => props.setShowForm(false)}>
+            {" "}
+            &times;{" "}
+          </span>
+          <Zoom left>
+            <form onSubmit={props.submitOrder}>
+              <Input
+                label="Name"
+                type="text"
+                name="name"
+                onChange={props.handleChange}
+              />
+              <Input
+                label="Email"
+                type="email"
+                onChange={props.handleChange}
+                name="email"
+              />
+
+              <div>
+                <button type="submit"> Checkout </button>
+              </div>
+            </form>
+          </Zoom>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Checkout
+export default Checkout;
