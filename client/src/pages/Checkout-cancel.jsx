9df067c8894a0ab763bfbdef3977044f1d4b7371
eng-mostafa-sayed <ms519197@gmail.com>
2023-02-12 18:16:52 +0200

    .

--- client/src/pages/Checkout-cancel.jsx
+++ client/src/pages/Checkout-cancel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const ChekoutCancel = () => {
+const CheckoutCancel = () => {
   return <div>Canceled</div>;
 };
 
-export default ChekoutCancel;
+export default CheckoutCancel;
--- client/src/pages/Chekout-success.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ChekoutSuccess = () => {
-  return <div>paid successfully</div>;
-};
-
-export default ChekoutSuccess;
